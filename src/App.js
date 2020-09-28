3116f453e4a72f59cb487463df6e2ba74db74eb8
Saltanat Nareshova <saltanatnareshova@Saltanats-MacBook-Pro.local>
2020-09-28 10:08:26 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Logistics from './components/logistics';
 import Contact from './components/contact';
 import {BrowserRouter} from 'react-router-dom';
 import { Route } from 'react-router-dom/cjs/react-router-dom.min';
+import Footer from './components/footer';
 
 class App extends React.Component{
   render(){
@@ -21,6 +22,7 @@ class App extends React.Component{
           <Route path='/services' component={Services} />
           <Route path='/logistics' component={Logistics} />
           <Route path='/contact' component={Contact} />
+          <Footer/>
         </div>
       </BrowserRouter>
       
---
+++ src/components/footer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './fotter.css';
+
+class Footer extends Component{
+    render(){
+        return(
+            <div className="footer">
+                <p className="footertext">© 2019 GSR Phoenix LLP</p>
+            </div>
+        );
+    }
+}
+
+export default Footer;
